68cdc8d69fa86ce10bbba4d148556f0692d5b128
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2021-10-23 17:17:16 -0500

    Se agrega endpoint de subida de archivos

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask,jsonify, request
+from dao.models import *
+from flask_cors import CORS
+
+import os
+import boto3
 
 app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+bucketname=''
+@app.route("/registroPorRUT",methods=['POST'])
+def registerByRUT():
+    
+    """
+    Ruta para subir imágenes de productos
+    """
+    headers=request.headers
+    fileName=headers.get('fileName')
+    print(request.files)
+    if 'file' not in request.files:
+        error = "Missing data source!"
+        return jsonify({'error': error})
+    file = request.files['file']
+    destination = '/tmp/'+fileName
+    file.save(destination)
+    success = "Success!"
+    s3 = boto3.resource('s3')
+    data={
+		'file':destination,
+		'bucket':bucketname,
+		'path':'users/'+fileName
+	}
+    s3.meta.client.upload_file(data['file'],data['bucket'] , data['path'])
+    return jsonify({'file': success})
+
 
 if __name__=="__main__":
     app.run(debug=True)
