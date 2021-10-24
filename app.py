9761f5a4dd9b17b55cb332d1d6eab90b64629af6
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2021-10-23 20:42:02 -0500

    se agrega ruta de subida de audio

--- app.py
+++ app.py
@@ -35,6 +35,30 @@ def registerByRUT():
     s3.meta.client.upload_file(data['file'],data['bucket'] , data['path'])
     return jsonify({'file': success})
 
+@app.route("/audio",methods=['POST'])
+def uploadAudio():
+    
+    """
+    Ruta para subir imágenes de productos
+    """
+    headers=request.headers
+    fileName=headers.get('fileName')
+    print(request.files)
+    if 'file' not in request.files:
+        error = "Missing data source!"
+        return jsonify({'error': error})
+    file = request.files['file']
+    destination = fileName
+    file.save(destination)
+    success = "Success!"
+    s3 = boto3.resource('s3')
+    data={
+		'file':destination,
+		'bucket':bucketname,
+		'path':'users/'+fileName
+	}
+    s3.meta.client.upload_file(data['file'],data['bucket'] , data['path'])
+    return jsonify({'file': success})
 
 if __name__=="__main__":
     app.run(debug=True)
